06186129d8469b8111f49d2969c3da1d8a799bc4
Sturek <Sturek@users.noreply.github.com>
2020-09-25 23:05:34 +0200

    Create derbonk

---
+++ keyboards/sofle/keymaps/derbonk/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+//Underglow
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 14
+#define RGBLED_SPLIT {7,7}
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_TWINKLE
