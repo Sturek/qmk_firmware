928ecf86041c3b244c34cc6518b85d4585e741c0
Sturek <Sturek@users.noreply.github.com>
2021-04-19 21:04:49 +0200

    Some other keymaps

--- keyboards/sofle/keymaps/Launch/config.h
+++ keyboards/sofle/keymaps/Launch/config.h
@@ -5,6 +5,8 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
-
-#define ENCODER_RESOLUTIONS { 4, 2 }
+//Add RGB underglow
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 14
+#define RGBLED_SPLIT {7,7}
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
